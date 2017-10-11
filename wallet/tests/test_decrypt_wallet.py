1059c5d244e8dd5b14a5070011a9d1a92c95e47a
Viet Le <vietlq85@gmail.com>
2017-10-11 20:23:25 +0100

    misc

--- wallet/tests/test_decrypt_wallet.py
+++ wallet/tests/test_decrypt_wallet.py
@@ -1,11 +1,13 @@
 import pytest
 import os, sys
 
-import decrypt_utc
+from decrypt_utc import decrypt_utc_file
 
-def decrypt_v3_scrypt_aes_128_ctr():
+def test_decrypt_v3_scrypt_aes_128_ctr():
     utc_file_name = 'test_v3_scrypt_aes_128_ctr_utc.json'
-    with open('test_v3_scrypt_aes_128_ctr_pwd.txt') as pwd_fh:
-        pwd = pwd_fh.read()
+    utc_pwd_file_name = 'test_v3_scrypt_aes_128_ctr_pwd.txt'
+    with open(utc_pwd_file_name, 'r') as pwd_fh:
+        pwd = pwd_fh.read().strip()
     print('pwd =', pwd)
+    assert len(pwd) == 64
     decrypt_utc_file(pwd, utc_file_name)
