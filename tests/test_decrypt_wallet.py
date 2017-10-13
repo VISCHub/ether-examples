11fdc2360865883d70239c7f6e92fbed51c826e3
Le Quoc Viet <vietlq85@gmail.com>
2017-10-13 09:08:24 +0100

    Update test_decrypt_wallet.py

--- tests/test_decrypt_wallet.py
+++ tests/test_decrypt_wallet.py
@@ -9,7 +9,9 @@ params_v3_scrypt_aes_128_ctr = {
     'pwd_file': 'tests/test_v3_scrypt_aes_128_ctr_pwd.txt',
 }
 
+
 def test_decrypt_v3_scrypt_aes_128_ctr():
+    '''Test decryption on UTC json file encrypted with AES 128 CTR'''
     utc_file_name = params_v3_scrypt_aes_128_ctr['utc_file']
     utc_pwd_file_name = params_v3_scrypt_aes_128_ctr['pwd_file']
 
